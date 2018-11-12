d30ad593790752892aa24a1ca938037f2cb4efb9
kacper <mitregakacper@gmail.com>
2018-11-12 23:48:55 +0100

    delay function changed

--- lab5/src/Main.java
+++ lab5/src/Main.java
@@ -1,54 +1,48 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String pathIn;
         String pathOut;
         int delay_ms;
-        int framerate;
+        int fps;
         try {
             pathIn = args[0];
             pathOut = args[1];
             delay_ms = Integer.parseInt(args[2]);
-            framerate = Integer.parseInt(args[3]);
-            /*test 
+            fps = Integer.parseInt(args[3]);
+            /*test
             pathIn = "C:\\Users\\Admin\\Desktop\\gravity.txt";
             pathOut = "C:\\Users\\Admin\\Desktop\\wynik.txt";
             delay_ms = 2000;
-            framerate = 60;*/
+            fps = 60;*/
         } catch (ArrayIndexOutOfBoundsException e) {
             System.err.println("Wrong args");
             return;
         }
-        if (framerate < 0) {
+        if (fps < 0) {
             System.err.println("Framerate can't be negative");
         }
-        File in = new File(pathIn);
-        PrintWriter OutputFile = null;
+        PrintWriter outputFile = null;
         try {
-            OutputFile = new PrintWriter(pathOut);
+            outputFile = new PrintWriter(pathOut);
         } catch (FileNotFoundException e) {
             System.err.println("Invalid Path: " + pathOut);
             return;
         }
-        Scanner sc = null;
+        File in = new File(pathIn);
+
         try {
-            sc = new Scanner(in);
-        } catch (FileNotFoundException e) {
-            System.err.println("Invalid Path: " + pathIn);
+            Subtitles.delay(in, outputFile, delay_ms, fps);
+        } catch (InvalidFormat e) {
+            System.err.println(e.getMessage());
             return;
+        } catch (FileNotFoundException e){
+            System.err.println(e.getMessage());
         }
-        while (sc.hasNextLine()) {
-            try {
-                OutputFile.println(Subtitles.delay(sc.nextLine(), delay_ms, framerate));
-            } catch (InvalidFormat e) {
-                System.err.println(e.getMessage());
-                return;
-            }
-        }
-        OutputFile.close();
+
+        outputFile.close();
     }
 }
--- lab5/src/Subtitles.java
+++ lab5/src/Subtitles.java
@@ -1,18 +1,31 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.io.PrintWriter;
+import java.io.File;
 
 public class Subtitles {
-    public static String delay(String in,int delay, int fps) throws InvalidFormat{
+    public static void delay(File in,PrintWriter out, int delay, int fps) throws InvalidFormat,
+            FileNotFoundException {
         String pattern = "\\{(\\d+)\\}\\{(\\d+)\\}(.+)";
         Pattern correct = Pattern.compile(pattern);
-        Matcher m = correct.matcher(in);
-        StringBuilder result = new StringBuilder();
+        Scanner scanner = new Scanner(in);
 
-        if(m.find()) {
-            int newStart = Integer.parseInt(m.group(1)) + (delay * fps) / 1000;
-            int newEnd = Integer.parseInt(m.group(2)) + (delay * fps) / 1000;
-            result.append("{"+newStart+"}{"+newEnd+"}"+m.group(3));
-            return result.toString();
-        } else throw new InvalidFormat(in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            Matcher m = correct.matcher(line);
+            StringBuilder result = new StringBuilder();
+            if (m.find()) {
+                int newStart = Integer.parseInt(m.group(1)) + (delay * fps) / 1000;
+                int newEnd = Integer.parseInt(m.group(2)) + (delay * fps) / 1000;
+                if (newEnd < newStart) {
+                    throw new InvalidFormat(line);
+                }
+                result.append("{" + newStart + "}{" + newEnd + "}" + m.group(3));
+                out.println(result.toString());
+            } else throw new InvalidFormat(line);
+
+        }
     }
 }
